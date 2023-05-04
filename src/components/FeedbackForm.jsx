2c3f6b760d4f1037e4118a0c2282fe3cd8e5cc03
junaid-at-092026438222 <oKhkolg8boM4SvOhg3+KbLMd39jkHadSAOmWkhmCeT4=>
2023-05-05 01:48:00 +0500

    Implement real time validation on feedback form field + implement character length + handle button

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,9 +4,35 @@ import Button from './shared/Button';
 
 const FeedbackForm = () => {
 
-  const [text, setText] = useState(''); 
+  const [text, setText] = useState('');
+
+  const [btnDisabled, setBtnDisabled] = useState(true); // send button is disabled until the length is not meet 
+  
+  const [message, setMessage] = useState(''); 
 
   const handleTextChange = (event) => {
+
+    if (text === '') {
+
+        setBtnDisabled(true);
+        
+        setMessage(null);
+    
+    } else if (text !== '' && text.trim().length <= 10) {
+
+        setMessage('Text must be atleast 10 characters');
+        
+        setBtnDisabled(true);
+    
+    } else {
+        
+        // So if there is atleast 10 characters and something types in then we wil....
+
+        setBtnDisabled(false);
+        
+        setMessage(null);
+    }
+
     setText(event.target.value)
   }
   
@@ -20,13 +46,17 @@ const FeedbackForm = () => {
             <div className="input-group">
                 <input 
                     type="text"
-                    value={text} 
+                    value={text}
                     onChange={handleTextChange} 
                     placeholder="Write a review" 
                 />
                 
-                <Button type="submit">Send</Button>
+                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
             </div>
+
+            {
+                message && <div className='message'> {message} </div>
+            }
         </form>
     </Card>
   )
